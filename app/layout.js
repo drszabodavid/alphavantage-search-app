fc795d9ede9e55e046b6b2ae028bd0419a8a08c7
David Szabo <david.szabo@firegize.com>
2023-07-23 22:54:18 +0200

    add header with home button

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>Header</header>
+        <header>
+          <nav className="h-20 bg-emerald-200 flex items-center justify-center">
+            <h1 className="text-3xl">Alpavantage Search Tool</h1>
+            <Link className="absolute left-5" href="/">
+              Home
+            </Link>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
