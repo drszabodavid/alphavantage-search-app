974e5db16d1ea51a0df0c81d875606b021316542
David Szabo <david.szabo@firegize.com>
2023-07-23 23:21:50 +0200

    remove home button and make title clickable

--- app/layout.js
+++ app/layout.js
@@ -14,10 +14,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <header>
-          <nav className="h-20 bg-emerald-200 flex items-center justify-center">
-            <h1 className="text-3xl">Alpavantage Search Tool</h1>
-            <Link className="absolute left-5" href="/">
-              Home
+          <nav className="h-24 bg-emerald-200 flex items-center justify-center">
+            <Link className="lg:text-center lg:relative" href="/">
+              <h1 className="md:text-2xl lg:text-4xl">
+                Alpavantage Search Tool
+              </h1>
             </Link>
           </nav>
         </header>
