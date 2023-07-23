bd77d5f5bc079b134a69fceebdd1b858fd31c66f
David Szabo <david.szabo@firegize.com>
2023-07-23 21:30:19 +0200

    add most traded stocks fetch for default data

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import getTopStocks from "@/lib/getTopStocks";
+import getMostTradedStocks from "@/lib/getMostTradedStocks";
 
 export default async function Home() {
-  const defaultData = await getTopStocks();
-  
+  const defaultData = await getMostTradedStocks();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Home
