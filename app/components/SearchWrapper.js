1ba6a5d19c50dbe3b47ac8c3acfff7cb57b1b5eb
David Szabo <david.szabo@firegize.com>
2023-07-24 11:58:01 +0200

    create SearchWrapper for client side

---
+++ app/components/SearchWrapper.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+import StockList from "./StockList";
+
+export default function SearchWrapper({ defaultData }) {
+  const [query, setQuery] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+
+  return (
+    <div className="flex flex-col items-center justify-between px-4 pb-8">
+      <SearchBar
+        query={query}
+        setQuery={setQuery}
+        setNoResults={setNoResults}
+        shouldEmpty={searchData.length}
+        getSearchResults={(results) => {
+          setSearchData(results);
+          setNoResults(results.length == 0);
+        }}
+      />
+      <StockList
+        noResults={noResults}
+        isSearch={!!searchData.length}
+        searchData={searchData}
+        defaultData={defaultData}
+      />
+    </div>
+  );
+}
