8d89fb7f39de9a89721d5f90b461902fb52b291c
David Szabo <david.szabo@firegize.com>
2023-07-24 11:57:28 +0200

    seperate search and default results & add noResults option

--- app/components/StockList.js
+++ app/components/StockList.js
@@ -1,19 +1,25 @@
+import SearchCard from "./SearchCard";
 import StockCard from "./StockCard";
 
-export default function StockList({ stocks }) {
+export default function StockList({
+  noResults,
+  isSearch,
+  defaultData,
+  searchData
+}) {
+  if (noResults) {
+    return <h3>No results</h3>;
+  }
+
   return (
     <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto gap-10">
-      {stocks.map((stock) => {
-        const { ticker, price, change_amount } = stock;
-        return (
-          <StockCard
-            key={ticker}
-            ticker={ticker}
-            price={price}
-            change_amount={change_amount}
-          />
-        );
-      })}
+      {isSearch
+        ? searchData?.map((stock) => {
+            return <SearchCard key={stock.ticker} stock={stock} />;
+          })
+        : defaultData?.map((stock) => {
+            return <StockCard key={stock.symbol} stock={stock} />;
+          })}
     </ul>
   );
 }
